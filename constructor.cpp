55755ad1bbb6cc2bd43944ff3556b9bc5969f2c4
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-05-17 15:09:47 +0700

    Add Mahasiswa class with private attributes

--- constructor.cpp
+++ constructor.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Dosen; // Forward declaration
+class Universitas; // Forward declaration
+
+class Mahasiswa {
+private:
+    string nama;
+    string NIM;
+    float nilai;
