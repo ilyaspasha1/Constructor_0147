836424882172b644d02fc8b3cc3fe9b340cf69ca
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-05-17 15:48:22 +0700

    Add constructor and tampilkanInfo method to Mahasiswa

--- constructor.cpp
+++ constructor.cpp
@@ -10,3 +10,12 @@ private:
     string nama;
     string NIM;
     float nilai;
+
+public:
+    Mahasiswa(string n, string id) : nama(n), NIM(id), nilai(0) {}
+
+    void tampilkanInfo() {
+        cout << "Nama Mahasiswa: " << nama << endl;
+        cout << "NIM: " << NIM << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
